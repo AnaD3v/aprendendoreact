bc6ce1945af5c19f7da26c75dd36209d5048aa8e
Ana Flávia <ana-neves98@outlook.com>
2023-07-16 13:01:46 -0300

    aprendendo com react

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+export default function App() {
+  return (
+    <div class="container">
+      <img src="./react-img.png" alt="imgReact" class="img" />
+      <h1>React</h1>
+      <p>A biblioteca para a interfaces usuários web e nativas</p>
+      <div class="buttons">
+        <button>Aprenda React</button>
+        <button>Referência da API</button>
+      </div>
+      <hr></hr>
+      <h2>Crie interfaces de usuário e componentes</h2>
+      <p>
+        React permite que você construa interfaces de usuário a partir de
+        pedaços individuais chamados componentes
+      </p>
+      <hr />
+      <h2>Escreva componentes com código e marcação</h2>
+      <p>
+        Componentes React são funções JavaScript A sintaxe de marcação é chamada
+        de JSX É uma extensão da sintaxe JavaScript popularizada pelo React.
+      </p>
+      <hr />
+      <h2>Escreva componentes com código e marcação</h2>
+      <ul>
+        <li>Uso de dados dinámicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Reutilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso do eventos JavaScript</li>
+      </ul>
+    </div>
+  );
+}
